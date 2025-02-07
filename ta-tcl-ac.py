8ec13876f28747005dec1b338552b9159f1746ee
leenux <leenux@163.com>
2025-02-07 11:56:48 +0800

    Add files via upload

--- ta-tcl-ac.py
+++ ta-tcl-ac.py
@@ -14,24 +14,32 @@ def getLevelsByData(s):
     for i in range(13):
         sum += s[i]
         lvls += getLevelsByByte(s[i])
-    lvls += getLevelsByByte(0xF + sum)
+    lsb = swap_msb_lsb(0xF + sum)
+    lvls += getLevelsByByte(lsb)
     return lvls
+    
+def swap_msb_lsb(n):
+    binary_str = bin(n)[2:]
+    binary_str = binary_str.zfill(8)
+    swapped_binary_str = binary_str[-1] + binary_str[1:-1] + binary_str[0]
+    result = int(swapped_binary_str, 2)
+    return result
     
 if __name__ == "__main__":
     code = startLevel                   #引导码
-    code += getLevelsByData([0b00100011,    #0
-                            0b11001011,     #1
-                            0b00100110,     #2
-                            0b00000010,     #3
+    code += getLevelsByData([0b11000100,    #0
+                            0b11010011,     #1
+                            0b01100100,     #2
+                            0b01000000,     #3
                             0b00000000,     #4
-                            0b00000100,     #5
-                            0b00001000,     #6
-                            0b00000101,     #7
+                            0b00100000,     #5
+                            0b11000000,     #6
+                            0b10100000,     #7
                             0b00000000,     #8
                             0b00000000,     #9
                             0b00000000,     #10
                             0b00000000,     #11
-                            0b10000000])    #12
+                            0b00000001])    #12
     code += endLevel                   #结束码
     print("电平码：")
     print(code)
